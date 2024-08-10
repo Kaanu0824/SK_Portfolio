967a879618eac262c2463358db28bf39a99b1a02
Kaanu0824 <Kaanu737@gmail.com>
2024-08-10 15:07:46 +0530

    Contact Message Error Fixed

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -6,8 +6,9 @@ import Insta from '../../assets/ins.png';
 import Linke from '../../assets/linke.png';
 import './contact.css';
 
-const Contact = ()=> {
+const Contact = () => {
   const form = useRef();
+
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -18,45 +19,46 @@ const Contact = ()=> {
       .then(
         () => {
           console.log('SUCCESS!');
-        },(error) => {
+          alert('Message sent successfully!'); 
+          form.current.reset(); 
+        },
+        (error) => {
           console.log('FAILED...', error.text);
+          alert('Failed to send the message. Please try again.'); 
         },
       );
   };
 
   return (
     <span id='contactpage'>
-        <div id ='contact'>
-            <h1 className='contactPageTitle'> Contact  Me  </h1>
-            <span className='clientDesc'> Please fill out the form below to discuss any work opportunities </span>
+      <div id='contact'>
+        <h1 className='contactPageTitle'>Contact Me</h1>
+        <span className='clientDesc'>Please fill out the form below to discuss any work opportunities</span>
 
-            <form className='contactForm' ref={form} onSubmit={sendEmail} >
-              
-                <input type='text' className='name' placeholder='Your Name' name="user_name" />
-                <input type='email' className='email' placeholder='Your Email' name="user_email" />
-                <textarea name='message' rows='5' className='msg' placeholder='Your Feedback' /> 
-                <button type='submit' value='Send' className='submitBtn'> Submit </button>
+        <form className='contactForm' ref={form} onSubmit={sendEmail}>
+          <input type='text' className='name' placeholder='Your Name' name='user_name' required />
+          <input type='email' className='email' placeholder='Your Email' name='user_email' required />
+          <textarea name='message' rows='5' className='msg' placeholder='Your Feedback' required />
+          <button type='submit' className='submitBtn'>Submit</button>
 
-
-                <div className='links'>
-                    <a href='https://www.linkedin.com/in/kaanushan-sivarasa/' target='_blank' rel='noopener noreferrer'>
-                        <img src={Linke} alt='LinkedIn' className='link'/>
-                    </a>
-                    <a href='https://github.com/Kaanu0824' target='_blank' rel='noopener noreferrer'>
-                    <img src={Git} alt='GitHub' className='link'/>
-                    </a>
-                    <a href='https://web.facebook.com/KAANUSHAN' target='_blank' rel='noopener noreferrer'>
-                    <img src={Face} alt='Facebook' className='link'/>
-                    </a>
-                    <a href='https://www.instagram.com/kaanu_sk_/' target='_blank' rel='noopener noreferrer'>
-                    <img src={Insta} alt='Instagram' className='link'/>
-                    </a>
-                </div>
-
-            </form>
-        </div>
+          <div className='links'>
+            <a href='https://www.linkedin.com/in/kaanushan-sivarasa/' target='_blank' rel='noopener noreferrer'>
+              <img src={Linke} alt='LinkedIn' className='link' />
+            </a>
+            <a href='https://github.com/Kaanu0824' target='_blank' rel='noopener noreferrer'>
+              <img src={Git} alt='GitHub' className='link' />
+            </a>
+            <a href='https://web.facebook.com/KAANUSHAN' target='_blank' rel='noopener noreferrer'>
+              <img src={Face} alt='Facebook' className='link' />
+            </a>
+            <a href='https://www.instagram.com/kaanu_sk_/' target='_blank' rel='noopener noreferrer'>
+              <img src={Insta} alt='Instagram' className='link' />
+            </a>
+          </div>
+        </form>
+      </div>
     </span>
-  )
-}
+  );
+};
 
 export default Contact;
