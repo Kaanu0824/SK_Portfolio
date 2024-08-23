8aaae0766f2d431a4788a6d523705f36f5dec8f8
Kaanu0824 <Kaanu737@gmail.com>
2024-08-23 12:02:55 +0530

    contact us Changes

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import Face from '../../assets/face.png';
-import Git from '../../assets/github.jpeg';
-import Insta from '../../assets/ins.png';
 import bg from '../../assets/intro-1.png';
-import Linke from '../../assets/linke.png';
-import What from '../../assets/What.webp';
 import './intro.css';
 
 function Intro() {
@@ -26,23 +21,6 @@ function Intro() {
         >
           <button className="btn"> Download Resume</button>
         </a>
-        <div className='links'>
-            <a href='https://www.linkedin.com/in/kaanushan-sivarasa/' target='_blank' rel='noopener noreferrer'>
-              <img src={Linke} alt='LinkedIn' className='link' />
-            </a>
-            <a href='https://github.com/Kaanu0824' target='_blank' rel='noopener noreferrer'>
-              <img src={Git} alt='GitHub' className='link' />
-            </a>
-            <a href='https://wa.me/+94774549953' target='_blank' rel='noopener noreferrer'>
-              <img src={What} alt='Whatsapp' className='link' />
-            </a>
-            <a href='https://www.instagram.com/kaanu_sk_/' target='_blank' rel='noopener noreferrer'>
-              <img src={Insta} alt='Instagram' className='link' />
-            </a>
-            <a href='https://web.facebook.com/KAANUSHAN' target='_blank' rel='noopener noreferrer'>
-              <img src={Face} alt='Facebook' className='link' />
-            </a>
-          </div>
       </div>
       <img src={bg} alt="Background" className="bg" />
     </section>
