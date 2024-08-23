2c4496b5fc92a234d753af213bab5c47c4dd4f42
Kaanu0824 <Kaanu737@gmail.com>
2024-08-23 11:45:43 +0530

    align changes

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import "./footer.css"
 function Footer() {
   return (
 
-    <span className='footer' > Copyright &#169; 2024 STK. All right received. </span>
+    <span className='footer' > Copyright &#169; 2024 SK. All right received. </span>
   )
 }
 
